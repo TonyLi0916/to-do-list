78c9583f06eba07f8b70774de2cb31865101564a
TonyLi0916 <tonyli4505@gmail.com>
2025-11-14 18:20:28 -0500

    updated configuration and files

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,10 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     clean: true,
   },
+  devtool: "eval-source-map",
+  devServer: {
+    watchFiles: ["./src/template.html"],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
@@ -18,9 +22,17 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(png|svg|jpg|jpeg|avif|gif)$/i,
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-    ]
-}
+    ],
+  },
 };
