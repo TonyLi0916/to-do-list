9a870f60ff1957de30b239cf4bec0b0365f3f24e
TonyLi0916 <tonyli4505@gmail.com>
2025-11-14 19:09:42 -0500

    reading and saving to localstorage api

--- src/dom.js
+++ src/dom.js
@@ -2,3 +2,5 @@ const todoForm = document.querySelector("#to-do-form");
 const todoList = document.querySelector("#to-do-container");
 
 
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
 import "./styles.css";
+import Project from "./project.js";
+import Todo from "./todo.js";
+
